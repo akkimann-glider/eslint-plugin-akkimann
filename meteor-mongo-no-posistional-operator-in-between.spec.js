fdf216db499976a7f20dc019d001519bbf96f5d7
Akhil Mannuru <82356563+akkimann-glider@users.noreply.github.com>
2022-08-03 17:20:59 +0530

    Update meteor-mongo-no-posistional-operator-in-between.spec.js

--- meteor-mongo-no-posistional-operator-in-between.spec.js
+++ meteor-mongo-no-posistional-operator-in-between.spec.js
@@ -3,10 +3,10 @@ const invalidPositionalOperator = require('./meteor-mongo-no-posistional-operato
 const ruleTester = new RuleTester();
 ruleTester.run('meteor-mongo-no-posistional-operator-in-between', invalidPositionalOperator, {
     valid: [{
-        code: 'db.collectionname.findOne({}, { fields: { _id: 1 } }) || {}',
+        code: 'db.collectionname.findOne({}, { fields: { "some.field.$": 1 } }) || {}',
     }],
     invalid: [{
-        code: 'db.collectionname.findOne({},{ fields: { "cardData.assessments.$.": 1 } })',
+        code: 'db.collectionname.findOne({},{ fields: { "some.field.$.": 1 } })',
         // we can use messageId from the rule object
         errors: [{ messageId: 'invalidPositionalOperator' }],
     }]
